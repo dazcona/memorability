34566bda8555727bae693b25e8d3fdc85102ef05
Enric Moreu <enric.moreu.filella@gmail.com>
2019-09-17 13:00:05 +0100

    adapt modelling for long-term

--- src/modelling.py
+++ src/modelling.py
@@ -1,9 +1,11 @@
 # imports
+import os
 import numpy as np
 from sklearn.svm import SVR
 from sklearn.linear_model import BayesianRidge, LinearRegression, ElasticNetCV, LassoCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
+import config
 
 
 ALGORITHMS = {
@@ -18,22 +20,39 @@ ALGORITHMS = {
         }
     },
     'SVM': {
-        'model': SVR(),
+        'model': SVR(gamma='scale'),
         'params': {
-            "kernel": ['linear', 'rbf', ], # 'poly', 'sigmoid'
+            "kernel": ['linear', 'rbf' ], # 'poly', 'sigmoid'
             "C": [0.1, 1.0, 100.0, 10000.0],
             "epsilon": [0.001, 1.0],
         }
     }
 }
 BEST_MODEL = {
-    'C3D': BayesianRidge(lambda_2=1e-06, alpha_1=1e-06, alpha_2=0.1, normalize=False, lambda_1=0.1),
-    'AESTHETICS': BayesianRidge(lambda_2=0.1, alpha_1=0.1, alpha_2=1e-06, normalize=True, lambda_1=1e-06),
-    'HMP': SVR(kernel='linear', C=100.0, epsilon=0.001),
-    'ColorHistogram': SVR(kernel='rbf'),
-    'LBP': BayesianRidge(lambda_2=0.1, alpha_1=0.1, alpha_2=1e-06, normalize=False, lambda_1=1e-06),
-    'InceptionV3': BayesianRidge(),
-    'ResNet152': SVR(kernel='rbf', C=0.1, epsilon=0.001),
+    'short-term_memorability': {
+        'C3D': BayesianRidge(lambda_2=1e-06, alpha_1=1e-06, alpha_2=0.1, normalize=False, lambda_1=0.1),
+        'AESTHETICS': BayesianRidge(lambda_2=0.1, alpha_1=0.1, alpha_2=1e-06, normalize=True, lambda_1=1e-06),
+        'HMP': SVR(kernel='linear', C=100.0, epsilon=0.001, gamma='scale'),
+        'ColorHistogram': SVR(kernel='rbf', gamma='scale'),
+        'LBP': BayesianRidge(lambda_2=0.1, alpha_1=0.1, alpha_2=1e-06, normalize=False, lambda_1=1e-06),
+        'InceptionV3': BayesianRidge(),
+        'CAPTIONS': SVR(gamma='scale'),
+        'ResNet50': SVR(gamma='scale'),
+        'ResNet152': SVR(kernel='rbf', C=0.1, epsilon=0.001, gamma='scale'),
+        'ResNet152V2': SVR(gamma='scale'),
+    },
+    'long-term_memorability' : {
+        'C3D': SVR(C=1.0, kernel='rbf', epsilon=0.001, gamma='scale'),
+        'AESTHETICS': BayesianRidge(lambda_2=1e-06, alpha_1=1e-06, alpha_2=0.1, normalize=True, lambda_1=0.1),
+        'HMP': SVR(kernel='linear', epsilon=0.001, C=100.0, gamma='scale'),
+        'ColorHistogram': BayesianRidge(lambda_2=0.0001, alpha_1= 1e-06, alpha_2=0.1, normalize=True, lambda_1=0.1),
+        'LBP': BayesianRidge(lambda_2=0.0001, alpha_1= 0.1, alpha_2=1e-06, normalize=True, lambda_1=1e-06),
+        'InceptionV3': BayesianRidge(),
+        'CAPTIONS': SVR(gamma='scale'),
+        'ResNet50': SVR(kernel='rbf', C=0.1, epsilon=0.001, gamma='scale'),
+        'ResNet152': SVR(kernel='rbf', C=0.1, epsilon=0.001, gamma='scale'),
+        'ResNet152V2': SVR(gamma='scale'),
+    },
 }
 CV = 3
 
@@ -42,7 +61,9 @@ def train_predict(X_train, y_train, X_val, feature_name, method, grid_search=Fal
 
     # Model
     if grid_search:
-        print('[INFO] Performing Grid Search for {}...'.format(method))
+        print('[INFO] Performing Grid Search for {} on {}...'.format(method, feature_name))
+        with open(os.path.join(config.RUN_LOG_DIR, 'mainlog.txt'), 'a') as f:
+            print('[INFO] Performing Grid Search for {} on {}...'.format(method, feature_name), file=f)
         # Perform grid search
         model = GridSearchCV(
             ALGORITHMS[method]['model'],
@@ -52,7 +73,9 @@ def train_predict(X_train, y_train, X_val, feature_name, method, grid_search=Fal
         )
     else:
         print('[INFO] Initializing model for {}...'.format(feature_name))
-        model = BEST_MODEL[feature_name]
+        with open(os.path.join(config.RUN_LOG_DIR, 'mainlog.txt'), 'a') as f:
+            print('[INFO] Initializing model for {}...'.format(feature_name), file=f)
+        model = BEST_MODEL[config.TARGET][feature_name]
 
     # Fit model
     print('[INFO] Fitting model...')
@@ -63,6 +86,10 @@ def train_predict(X_train, y_train, X_val, feature_name, method, grid_search=Fal
         print("[INFO] best hyperparameters: {}".format(model.best_params_))
         with open(os.path.join(config.RUN_LOG_DIR, 'mainlog.txt'), 'a') as f:
             print("""GRID SEARCH: Best hyperparameters for {}: {})""".format(method, model.best_params_), file=f)
+    else:
+        print("[INFO] Parameters: {}".format(model.get_params()))
+        with open(os.path.join(config.RUN_LOG_DIR, 'mainlog.txt'), 'a') as f:
+            print("""MODEL: Parameters for {}: {})""".format(method, model.get_params()), file=f)
 
     # Predict
     print('[INFO] Predict new values...')
